3c1caaa16ec94d91b7b0bf4c4f734e5d20b54d33
NimayRKumar <nimay.r.kumar@gmail.com>
2023-12-13 19:46:30 -0500

    updated script for creating dataset

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import librosa
 import librosa.display
 import os
 import shutil
-
-from PIL import Image
+import json
+from ast import literal_eval
 
 #replace with your own if using absolute paths, or empty string if not
 data_dir = '/Users/nimay/Desktop/repos/in_the_jungle/data'
+label_encoding = json.load(open('./label_encoding.JSON'))
 FIG_SIZE = (15,10)
 
 def rename_data(rootdir):
@@ -18,21 +20,21 @@ def rename_data(rootdir):
                 new = file.split('_')[1]
                 shutil.move(os.path.join(rootdir, subdir, file), os.path.join(rootdir, subdir, new))
 
-
+# Pandas df: one column for signal, one for label
+# TODO: save padded signal
 def create_spectrogram(file, out_path):
+    file_path = file.split("/")[-1][:-4]
+    fig = plt.Figure()
+
     # load audio file with Librosa
     signal, sample_rate = librosa.load(file, sr=22050)
+    return signal
 
-    # WAVEFORM
-    # display waveform
     plt.figure(figsize=FIG_SIZE)
     librosa.display.waveshow(signal, sr=sample_rate, color="blue")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Amplitude")
-    plt.title("Waveform")
+    plt.savefig("{}/signal/{}".format(out_path, file_path))
 
     # FFT -> power spectrum
-    # perform Fourier transform
     fft = np.fft.fft(signal)
 
     # calculate abs values on complex numbers to get magnitude
@@ -40,17 +42,6 @@ def create_spectrogram(file, out_path):
 
     # create frequency variable
     f = np.linspace(0, sample_rate, len(spectrum))
-
-    # take half of the spectrum and frequency
-    left_spectrum = spectrum[:int(len(spectrum)/2)]
-    left_f = f[:int(len(spectrum)/2)]
-
-    # plot spectrum
-    plt.figure(figsize=FIG_SIZE)
-    plt.plot(left_f, left_spectrum, alpha=0.4)
-    plt.xlabel("Frequency")
-    plt.ylabel("Magnitude")
-    plt.title("Power spectrum")
 
     # STFT -> spectrogram
     hop_length = 512 # in num. of samples
@@ -66,9 +57,6 @@ def create_spectrogram(file, out_path):
     # calculate abs values on complex numbers to get magnitude
     spectrogram = np.abs(stft)
 
-    file_path = file.split("/")[-1][:-4]
-    fig = plt.Figure()
-
     # Save to DB Spectrogram
     log_spectrogram = librosa.amplitude_to_db(spectrogram)
     
@@ -82,11 +70,22 @@ def create_spectrogram(file, out_path):
     librosa.display.specshow(MFCCs, sr=sample_rate, hop_length=hop_length)
     plt.savefig("{}/mfcc/{}".format(out_path, file_path))
 
-    exit(1)
 
-subdirs = next(os.walk(data_dir))[1]
-for sd in subdirs:
-    out_path = "{}/{}".format(data_dir, sd)
+def main(): 
+    df = pd.DataFrame(columns=["signal", "label"])
+    subdirs = next(os.walk(data_dir))[1]
+    for sd in subdirs:
+        out_path = "{}/{}".format(data_dir, sd)
 
-    for file in os.listdir("{}/wav".format(out_path)):
-        create_spectrogram("{}/wav/{}".format(out_path, file), out_path)
+        for file in os.listdir("{}/wav".format(out_path)):
+            sig = create_spectrogram("{}/wav/{}".format(out_path, file), out_path)
+            df.loc[len(df)] = [list(sig), label_encoding[sd]]
+
+    df.to_csv('./csv/dataset.csv')
+  
+
+if __name__=="__main__": 
+    main()
+    #df = pd.read_csv('./csv/dataset.csv')
+    #df['signal'] = df['signal'].apply(literal_eval)
+
